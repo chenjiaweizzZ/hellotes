524c1cca66367f558f6511784614817feeba36a9
陈家伟 <19650@163.com>
2024-11-10 20:20:11 -0500

    04

---
+++ 04/index.ts
@@ -0,0 +1,5 @@
+import { sum } from "./utils/math"
+import { type IDType, type IPerson } from "./utils/type"
+// import type {IDType, IPerson } from "./utils/type"
+console.log(sum(10, 20))
+const id1: IDType = 2336
